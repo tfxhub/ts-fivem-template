ca8d3e057979a5744d24e3c9e878bfcf187ecd86
Ian Ramos <102131182+ianram0s@users.noreply.github.com>
2024-03-08 07:55:22 -0300

    Fix typo

--- src/common/utils/fivem.ts
+++ src/common/utils/fivem.ts
@@ -15,7 +15,7 @@ export const isClient = () => {
 };
 
 /**
- * @returns True if fxmanifest 'env' metadata is set to 'prod'.
+ * @returns True if fxmanifest 'env' metadata is set to 'dev'.
  */
 export const isDev = () => {
     return GetResourceMetadata(GetCurrentResourceName(), 'env', 0) == 'dev';
