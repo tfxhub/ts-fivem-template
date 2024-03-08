c85b0501abcd01037ccfa697de6a72bedd025d99
Ian Ramos <contact@ianbytes.com>
2024-03-08 07:54:00 -0300

    Fix identation

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,9 +2,9 @@ import { helloWorld } from '@common/index';
 import { log } from '@common/utils';
 
 on('onResourceStart', async (name: string) => {
-	if (name != GetCurrentResourceName()) return;
+    if (name != GetCurrentResourceName()) return;
 
-	log.info('Server script started.');
+    log.info('Server script started.');
 
-	log.debug(helloWorld());
+    log.debug(helloWorld());
 });
