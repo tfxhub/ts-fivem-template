6fd1fa2d7e951567e3114a8c49907e41cf36e23a
Ian Ramos <102131182+ianram0s@users.noreply.github.com>
2024-03-08 16:48:16 -0300

    Utils update

--- src/common/utils/env.ts
+++ src/common/utils/env.ts
@@ -12,3 +12,19 @@ export const isDev = () => {
 export const isProd = () => {
     return GetResourceMetadata(GetCurrentResourceName(), 'env', 0) == 'prod';
 };
+
+/**
+ *
+ * @returns True if the function is running from a server-side script.
+ */
+export const isServer = () => {
+    return IsDuplicityVersion();
+};
+
+/**
+ *
+ * @returns True if the function is running from a client-side script.
+ */
+export const isClient = () => {
+    return !isServer();
+};
